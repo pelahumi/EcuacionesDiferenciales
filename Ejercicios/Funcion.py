14c9a4fb784286b9564f3bcf8a748109978cc47c
Pelayo <phuermij@myuax.com>
2022-10-11 22:41:37 +0200

    Creacion del init, y primer ejercicio

---
+++ Ejercicios/Funcion.py
@@ -0,0 +1,16 @@
+#Importamos la librería que nos ayudará a resolver las EDO
+import sympy
+
+def resolver(f):
+    #El parámetro f será la ecuación a resolver
+
+    #Primero definimos las incógnitas
+    x = sympy.Symbol("x")
+    y = sympy.Function("y")
+
+    #Usamos la función para resolver la ecuación
+    solucion = sympy.dsolve(y(x).diff(x) - f)
+
+    return solucion
+
+    
