50be21ca8b4cf153ea7b7d07e553d76b3a11ba65
Pelayo <phuermij@myuax.com>
2022-10-11 22:48:50 +0200

    Funcion ejercicio 1

---
+++ Ejercicios/Ejercicio1.py
@@ -0,0 +1,16 @@
+#Importamos todo el fichero para no tener que importar varias veces la misma libreria
+from Ejercicios.Funcion import *
+
+def ejercicio1():
+
+    x = sympy.Symbol("x")
+    y = sympy.Function("y")
+
+    #Definimos la ecuación 
+    f = (x**2*y - y) / (y + 1)
+
+    #Usamos la funcion que creamos para resolver las EDO
+    print(resolver(f))
+
+ejercicio1()
+
--- Ejercicios/Funcion.py
+++ Ejercicios/Funcion.py
@@ -7,7 +7,7 @@ def resolver(f):
     #Primero definimos las incógnitas
     x = sympy.Symbol("x")
     y = sympy.Function("y")
-
+    
     #Usamos la función para resolver la ecuación
     solucion = sympy.dsolve(y(x).diff(x) - f)
 
